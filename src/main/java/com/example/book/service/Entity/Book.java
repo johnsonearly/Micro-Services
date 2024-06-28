2ce66443507078a1998dc40ac8e2e3ceac8d2a98
johnsonearly <johnsonegbichi@gmail.com>
2024-06-28 15:15:20 +0100

    Initial commit for the book service project

---
+++ src/main/java/com/example/book/service/Entity/Book.java
@@ -0,0 +1,23 @@
+package com.example.book.service.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+
+    private String author;
+
+    private  String book_number;
+    private String book_title;
+
+
+}
